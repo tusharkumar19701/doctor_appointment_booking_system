a8163c5777a68c6b498d6087bab8828a9b2f9daf
Tushar Kumar <tusharkumar19701@gmail.com>
2025-08-22 15:52:57 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
             <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight'>Book Appointment <br /> With Trusted Doctors</p>
             <div className="flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light">
-                <img className='w-28' src={assets.group_profiles} />
+{/*                 <img className='w-28' src={assets.group_profiles} /> */}
                 <p>Simply browse through our extensive list of trusted doctors, <br className='hidden sm:block' /> schedule your appointment hassle-free.</p>
             </div>
             <a href="#speciality" className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>
